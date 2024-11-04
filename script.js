5f7b0ad2c95ccce5221aefcfa59a3231989e1300
lukagvenetadze <lukagvenetadze04@gmail.com>
2024-11-04 10:39:42 +0400

    commit1

---
+++ script.js
@@ -0,0 +1,40 @@
+let aNum = 4;
+const bNum = 10;
+
+if ( aNum > 4) {
+    console.log(' a is larger then a');
+} else if (aNum == 4) {
+    console.log(' a is equal to 4');
+} else {
+    console.log('4 is greater then a');
+}
+
+
+
+let task1Number = 4;
+
+if (task1Number * task1Number > 100) {
+    console.log('square is larger then 100');
+} else {
+    console.log('its bellow 100');
+}
+
+
+let task2Number = 555;
+
+if (task2Number > 50 && task2Number < 100) {
+    console.log('the number is between 50 and 100');
+} else if (task2Number < 50) {
+    console.log('the number is bellow 50');
+} else if (task2Number > 100) {
+    console.log('the number is greater then 100');
+} else {
+    console.log('the number is unknow');
+}
+
+
+let task3Number = 5;
+
+if (task3Number == 0 || task3Number < 0) {
+    console.log('number is broken');
+} else {console.log('all is good');}
